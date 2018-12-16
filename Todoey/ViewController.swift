269a95a4dac93953de1957fc4db3c9c628b16b15
Asımcan ALKAN <alkan97@windowslive.com>
2018-12-16 14:37:47 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Asımcan ALKAN on 16.12.2018.
+//  Copyright © 2018 Asımcan ALKAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
